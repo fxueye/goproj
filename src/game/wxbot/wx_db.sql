12150bafad06464d55a322c599b2e5fffdde2d11
skw <admin@example.com>
2018-07-16 16:11:23 +0800

    update sql

--- src/game/wxbot/wx_db.sql
+++ src/game/wxbot/wx_db.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 121
+Source Server         : 192.168.1.143
 Source Server Version : 50720
-Source Host           : 192.168.1.121:3306
+Source Host           : 192.168.1.143:3306
 Source Database       : wx_db
 
 Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-07-15 15:15:16
+Date: 2018-07-16 16:10:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,6 +23,11 @@ CREATE TABLE `stroke` (
   `UID` int(11) NOT NULL AUTO_INCREMENT,
   `Send` varchar(200) NOT NULL DEFAULT '',
   `Tel` varchar(200) NOT NULL DEFAULT '',
-  `Content` text NOT NULL,
+  `Content` text CHARACTER SET utf8mb4 NOT NULL,
+  `Timestamp` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of stroke
+-- ----------------------------
