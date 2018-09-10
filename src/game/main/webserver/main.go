1755d5a6187769aeac4685a8cf6980066cfbdedd
skw <kewei.shi@mail.tipcat.com>
2018-09-10 17:04:51 +0800

    a

--- src/game/main/webserver/main.go
+++ src/game/main/webserver/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"game/common/utils"
 	web "game/web"
 )
 
 func main() {
+	err := utils.SendMail("568669736@qq.com", "jyskjugscvgpbcde", "smtp.qq.com:587", "281431280@qq.com", "server crash", "你好", "")
+	if err != nil {
+		fmt.Print(err)
+	}
 	web.Run()
 }
